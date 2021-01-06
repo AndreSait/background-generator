b6dcfa20ebf281df557f7376406035534ba183c3
AndreSait <andresaiteppeland@gmail.com>
2021-01-06 10:17:33 +0100

    no comment

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
-import { wait } from '@testing-library/react';
 
 class App extends Component{
   constructor() {
